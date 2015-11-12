eddd0193936eb952ee895708a4bc5d925d586fe0
Julien Kassar <github@kassisol.com>
2015-11-12 07:52:16 -0500

    Fix main_interface fact

--- lib/facter/main_interface.rb
+++ lib/facter/main_interface.rb
@@ -6,6 +6,7 @@ Facter.add('main_interface') do
   setcode do
     begin
       main_interface = `netstat -r | awk '/^default/ { print $NF }'`.strip
+      main_interface = main_interface.gsub('/[^a-zA-Z0-9]/', '_')
     rescue
       main_interface = 'none'
     end
@@ -20,7 +21,7 @@ Facter.add('main_interface') do
   setcode do
     begin
       main_interface = `route get default | awk '/interface/ { print $2 }'`.strip
-      main_interface = main_interface.sub(':', '_')
+      main_interface = main_interface.gsub('/[^a-zA-Z0-9]/', '_')
     rescue
       main_interface = 'none'
     end
