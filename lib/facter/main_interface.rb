343fc62f1bda717d5b77334d1ea201f272a79d0b
Julien Kassar <github@kassisol.com>
2015-11-25 10:18:51 -0500

    Fix main_interface regex for Solaris

--- lib/facter/main_interface.rb
+++ lib/facter/main_interface.rb
@@ -21,7 +21,7 @@ Facter.add('main_interface') do
   setcode do
     begin
       main_interface = `route get default | awk '/interface/ { print $2 }'`.strip
-      main_interface = main_interface.gsub('/[^a-zA-Z0-9]/', '_')
+      main_interface = main_interface.gsub(/[^a-z0-9_]/i, '_')
     rescue
       main_interface = 'none'
     end
